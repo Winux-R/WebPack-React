743219bb271656140d547c4a145d03c96ec2743b
jinxin <jinxin@wisers.com>
2017-04-14 17:52:26 +0800

    利用webpack进行React项目的简单构建

---
+++ app/component.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Jinxin on 2017/4/7.
+ */
+'use strict';
+
+
+module.exports = function () {
+    var element = document.createElement('h1');
+
+    element.innerHTML = 'Hello world';
+
+    return element;
+};
